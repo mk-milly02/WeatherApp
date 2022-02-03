aeaec158e4bb3d2dfac363ba4e8f78ebb9b88ffd
Jonas Ababio <jonasagesmilly@outlook.com>
2022-02-03 20:53:07 +0000

    Implement geocoding extensions class

---
+++ WeatherApp.UI/City.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherApp.UI
+{
+    public class City
+    {
+        public string Name { get; set; }
+
+        public List<string> LocalNames { get; set; }
+
+        public string Longitude { get; set; }
+
+        public string Latitude { get; set; }
+
+        public string State { get; set; }
+    }
+}
---
+++ WeatherApp.UI/GeocodingExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace WeatherApp.UI
+{
+    public class GeocodingExtensions
+    {
+        private string GetApiKey()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            return config.GetSection("apikey").Value;
+        }
+
+        public async Task<(string latitude, string longitude)> GetCoordinatesAsync(string city)
+        {
+            string key = GetApiKey();
+
+            HttpClient client = new();
+
+            var response = 
+                await client.GetAsync($"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={key}");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            City x = JsonConvert.DeserializeObject<City>(responseBody);
+
+            return (x.Latitude, x.Longitude);
+        }
+    }
+}
